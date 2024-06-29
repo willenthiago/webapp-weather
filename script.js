39bfc8a691771a5fd88949c6333d0f1b91dd78f6
Willen Thiago <willenthiagoc@gmail.com>
2024-06-29 16:39:19 -0300

    fix (history api)
    Ajuste na requisição do history API e na função setWeekWeather

--- script.js
+++ script.js
@@ -25,13 +25,13 @@ const ozoneInfo = document.getElementById('ozone-info')
 const coInfo = document.getElementById('co-info')
 
 //  week-weather info
-const todayWeather = document.getElementById('today-weather-icon')
+const d1Weather = document.getElementById('day1-weather-icon')
 const d2Weather = document.getElementById('day2-weather-icon')
 const d3Weather = document.getElementById('day3-weather-icon')
 const d4Weather = document.getElementById('day4-weather-icon')
 const d5Weather = document.getElementById('day5-weather-icon')
-const todayMax = document.getElementById('week-today-max')
-const todayMin = document.getElementById('week-today-min')
+const day1Max = document.getElementById('week-today-max')
+const day1Min = document.getElementById('week-today-min')
 const day2Max = document.getElementById('day2-max')
 const day2Min = document.getElementById('day2-min')
 const day3Max = document.getElementById('day3-max')
@@ -49,23 +49,23 @@ let timeZoneId = null
 // definindo localização
 getLocation.addEventListener('click', () => {
     let city = ''
-    do{
+    do {
         // recebendo dado do usuário
-       city = prompt('Digite a sua cidade: ')
-    }while(city == '')
+        city = prompt('Digite a sua cidade: ')
+    } while (city == '')
 
     getCoord(city)
 })
-async function getCoord(city){
+async function getCoord(city) {
     // requisição da api de geolocalização para recuperar latitude e longitude da localização
     let geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${geoApiKey}`
 
-    try{    
+    try {
         const response = await fetch(geoUrl)
         const lugar = await response.json()
         // const city = null
 
-        if(lugar[0].local_names.pt){
+        if (lugar[0].local_names.pt) {
             city = lugar[0].local_names.pt
         } else {
             city = lugar[0].local_names.en
@@ -73,34 +73,55 @@ async function getCoord(city){
 
         const lat = lugar[0].lat
         const lon = lugar[0].lon
-        
-        getWeather(lat,lon,city)
-    } catch(error){
+
+        getWeather(lat, lon, city)
+    } catch (error) {
         alert('Não foi possível identificar o lugar de sua escolha. Tente novamente')
     }
 }
 
 // definindo clima atual
-async function getWeather(latitude,longitude,city) {
+async function getWeather(latitude, longitude, city) {
     // requisição da previsão de tempo através da latitude e longitude.
-    try{
-    let currentWeatherUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${latitude},${longitude}&lang=pt&aqi=yes&days=3`
+    try {
+        let currentWeatherUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${latitude},${longitude}&lang=pt&aqi=yes&days=3`
 
-    const resp = await fetch(currentWeatherUrl)
-    const weather = await resp.json()
-    
-    getLocation.innerHTML = `${city} - ${weather.location.region}`
+        const resp = await fetch(currentWeatherUrl)
+        const weather = await resp.json()
 
-    timeZoneId = weather.location.tz_id
-    
-    setCurrentWeather(weather)
-    setWeekWeather(weather)
-    setAirQualityIndex(weather)
-    setInterval(updateTime, 100) // atualiza horário atual a cada 1seg
+        getLocation.innerHTML = `${city} - ${weather.location.region}`
 
-    } catch (error){
+        timeZoneId = weather.location.tz_id
+
+        const date = new Date()
+        date.setDate(date.getDate() - 2)
+        const day = date.getDate()
+        const month = date.getMonth() + 1
+        const year = date.getFullYear()
+        const dt = `${year}-${month}-${day}`
+        const endDt = `${year}-${month}-${day + 1}`
+
+        console.log(day);
+        let historyWeatherUrl = `https://api.weatherapi.com/v1/history.json?key=${apiKey}&q=${latitude},${longitude}&dt=${dt}&end_dt=${endDt}`
+
+        const respHist = await fetch(historyWeatherUrl)
+        const histWeather = await respHist.json()
+
+        setCurrentWeather(weather)
+        setWeekWeather(weather, histWeather)
+        setAirQualityIndex(weather)
+        setInterval(updateTime, 100) // atualiza horário atual a cada 1seg
+
+    } catch (error) {
         console.log(`Erro ao buscar dados: ${error}`);
     }
+
+    try {
+
+    } catch (err) {
+        console.log(`Erro ao buscar dados: ${err}`);
+    }
+
 }
 
 function setCurrentWeather(weather) {
@@ -110,13 +131,11 @@ function setCurrentWeather(weather) {
     windSpeed.innerText = weather.current.wind_kph
     humidity.innerHTML = weather.current.humidity
     rainProb.innerHTML = weather.forecast.forecastday[0].day.daily_chance_of_rain
-    todayMax.innerText = weather.forecast.forecastday[0].day.maxtemp_c
-    todayMin.innerText = weather.forecast.forecastday[0].day.mintemp_c
 
     // formatando sunrise e sunset
     let sunrise = weather.forecast.forecastday[0].astro.sunrise.split(' ')
     let sunset = weather.forecast.forecastday[0].astro.sunset.split(' ')
-    if(sunset[1] == 'PM'){
+    if (sunset[1] == 'PM') {
         const sunsetTime = sunset[0].split(':')
         let sunsetHour = Number(sunsetTime[0])
         let sunsetMin = sunsetTime[1]
@@ -129,8 +148,8 @@ function setCurrentWeather(weather) {
 }
 
 // definindo horario atual - Sun time info
-function updateTime(){
-    if(timeZoneId){
+function updateTime() {
+    if (timeZoneId) {
         const date = new Date()
         // const hh = date.getHours()
         // const mm = date.getMinutes()
@@ -139,76 +158,70 @@ function updateTime(){
             timeStyle: 'short',
             timeZone: timeZoneId
         }).format(date)
-        
+
         document.getElementById('time-now').innerText = formattedTime
     }
 }
 
 // definindo clima da semana
-function setWeekWeather(weekWeather) {
+function setWeekWeather(weekWeather, historyWeather) {
 
-    todayWeather.setAttribute('src', `${weekWeather.forecast.forecastday[0].day.condition.icon}`)
-    todayWeather.setAttribute('title', `${weekWeather.forecast.forecastday[0].day.condition.text}`)
+    d1Weather.setAttribute('src', `${historyWeather.forecast.forecastday[0].day.condition.icon}`)
+    d1Weather.setAttribute('title', `${historyWeather.forecast.forecastday[0].day.condition.text}`)
 
-    d2Weather.setAttribute('src', `${weekWeather.forecast.forecastday[1].day.condition.icon}`)
-    d2Weather.setAttribute('title', `${weekWeather.forecast.forecastday[1].day.condition.text}`)
+    d2Weather.setAttribute('src', `${historyWeather.forecast.forecastday[1].day.condition.icon}`)
+    d2Weather.setAttribute('title', `${historyWeather.forecast.forecastday[1].day.condition.text}`)
 
-    d3Weather.setAttribute('src', `${weekWeather.forecast.forecastday[2].day.condition.icon}`)
-    d3Weather.setAttribute('title', `${weekWeather.forecast.forecastday[2].day.condition.text}`)
+    d3Weather.setAttribute('src', `${weekWeather.forecast.forecastday[0].day.condition.icon}`)
+    d3Weather.setAttribute('title', `${weekWeather.forecast.forecastday[0].day.condition.text}`)
 
-    /* d4Weather.setAttribute('src', `${weekWeather.forecast.forecastday[3].day.condition.icon}`)
-    d4Weather.setAttribute('title', `${weekWeather.forecast.forecastday[3].day.condition.text}`)
+    d4Weather.setAttribute('src', `${weekWeather.forecast.forecastday[1].day.condition.icon}`)
+    d4Weather.setAttribute('title', `${weekWeather.forecast.forecastday[1].day.condition.text}`)
 
-    d5Weather.setAttribute('src', `${weekWeather.forecast.forecastday[4].day.condition.icon}`)
-    d5Weather.setAttribute('title', `${weekWeather.forecast.forecastday[4].day.condition.text}`) */
+    d5Weather.setAttribute('src', `${weekWeather.forecast.forecastday[2].day.condition.icon}`)
+    d5Weather.setAttribute('title', `${weekWeather.forecast.forecastday[2].day.condition.text}`)
 
-    day2Max.innerText = weekWeather.forecast.forecastday[1].day.maxtemp_c
-    day2Min.innerText = weekWeather.forecast.forecastday[1].day.mintemp_c
-    day3Max.innerText = weekWeather.forecast.forecastday[2].day.maxtemp_c
-    day3Min.innerText = weekWeather.forecast.forecastday[2].day.mintemp_c
-    /*day4Max.innerText = weekWeather.forecast.forecastday
-     [3].day.maxtemp_c
-    day4Min.innerText = weekWeather.forecast.forecastday[3].day.mintemp_c
-    day5Max.innerText = weekWeather.forecast.forecastday[4].day.maxtemp_c
-    day5Min.innerText = weekWeather.forecast.forecastday[4].day.mintemp_c */
+    day1Max.innerText = historyWeather.forecast.forecastday[0].day.maxtemp_c
+    day1Min.innerText = historyWeather.forecast.forecastday[0].day.mintemp_c
+    day2Max.innerText = historyWeather.forecast.forecastday[1].day.maxtemp_c
+    day2Min.innerText = historyWeather.forecast.forecastday[1].day.mintemp_c
+    day3Max.innerText = weekWeather.forecast.forecastday[0].day.maxtemp_c
+    day3Min.innerText = weekWeather.forecast.forecastday[0].day.mintemp_c
+    day4Max.innerText = weekWeather.forecast.forecastday[1].day.maxtemp_c
+    day4Min.innerText = weekWeather.forecast.forecastday[1].day.mintemp_c
+    day5Max.innerText = weekWeather.forecast.forecastday[2].day.maxtemp_c
+    day5Min.innerText = weekWeather.forecast.forecastday[2].day.mintemp_c
 
     const today = new Date()
     const day = today.getDay()
     switch (day) {
         case 1:
-            document.getElementById('day3-title').innerText = 'Quarta'
-            document.getElementById('day4-title').innerText = 'Quinta'
-            document.getElementById('day5-title').innerText = 'Sexta'
-            break;
-        case 2:
-            document.getElementById('day3-title').innerText = 'Quinta'
-            document.getElementById('day4-title').innerText = 'Sexta'
-            document.getElementById('day5-title').innerText = 'Sábado'
-            break;
-        case 3:
-            document.getElementById('day3-title').innerText = 'Sexta'
-            document.getElementById('day4-title').innerText = 'Sábado'
-            document.getElementById('day5-title').innerText = 'Domingo'
-            break;
-        case 4:
-            document.getElementById('day3-title').innerText = 'Sábado'
-            document.getElementById('day4-title').innerText = 'Domingo'
-            document.getElementById('day5-title').innerText = 'Segunda'
-            break;
-        case 5:
-            document.getElementById('day3-title').innerText = 'Domingo'
-            document.getElementById('day4-title').innerText = 'Segunda'
-            document.getElementById('day5-title').innerText = 'Terça'
-            break;
-        case 6:
-            document.getElementById('day3-title').innerText = 'Segunda'
-            document.getElementById('day4-title').innerText = 'Terça'
+            document.getElementById('day1-title').innerText = 'Sábado'
             document.getElementById('day5-title').innerText = 'Quarta'
             break;
-        case 7:
-            document.getElementById('day3-title').innerText = 'Terça'
-            document.getElementById('day4-title').innerText = 'Quarta'
+        case 2:
+            document.getElementById('day1-title').innerText = 'Domingo'
             document.getElementById('day5-title').innerText = 'Quinta'
+            break;
+        case 3:
+            document.getElementById('day1-title').innerText = 'Segunda'
+            document.getElementById('day5-title').innerText = 'Sexta'
+            break;
+        case 4:
+            document.getElementById('day1-title').innerText = 'Terça'
+            document.getElementById('day5-title').innerText = 'Sábado'
+            break;
+        case 5:
+            document.getElementById('day1-title').innerText = 'Quarta'
+            document.getElementById('day5-title').innerText = 'Domingo'
+            break;
+        case 6:
+            document.getElementById('day1-title').innerText = 'Quinta'
+            document.getElementById('day5-title').innerText = 'Segunda'
+            break;
+        case 7:
+            document.getElementById('day1-title').innerText = 'Sexta'
+            document.getElementById('day5-title').innerText = 'Terça'
             break;
     }
 }
